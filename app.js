5cd26df33b733b01ac1052b37650be5433b49f83
nmehanma <nmehanma@gmail.com>
2021-01-05 19:04:43 -0500

    updating new and create route

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -28,6 +29,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new'); 
+})
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async(req,res) => {
@@ -41,6 +52,7 @@ app.get('/makecampground', async (req, res) => {
   res.send(camp)
 
 })
+
 
 app.listen(3000, () => {
   console.log('Serving on port 3000')
